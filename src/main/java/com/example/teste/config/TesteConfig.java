942be2b3af77fb48126b2754d35cbb5f96984af5
oseiasfilho11 <oseiasfilho99@gmail.com>
2022-04-09 15:46:29 -0300

    Payment, one-to-one association

--- src/main/java/com/example/teste/config/TesteConfig.java
+++ src/main/java/com/example/teste/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.teste.entites.Category;
 import com.example.teste.entites.Order;
 import com.example.teste.entites.OrderItem;
+import com.example.teste.entites.Payment;
 import com.example.teste.entites.Product;
 import com.example.teste.entites.User;
 import com.example.teste.entites.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 
